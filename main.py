8cbf242f0cd22efbdcc25c5b45d70a3fb01b48c3
seikhchilli <saurabhkrsingh2001@gmail.com>
2022-07-13 18:24:47 +0530

    commit

--- main.py
+++ main.py
@@ -1,25 +1,97 @@
 import pickle
 from objects import person
 from tmdb import tmdb
+import genre_ids
 
-# pickle_off = open("EmpID.pickle", 'rb')
-# EmpID = pickle.load(pickle_off)
-# print(EmpID)
+pickle_off = open("db.pickle", 'rb')
+db = pickle.load(pickle_off)
 
-print('\nWelcome to Movie Tracker\n')
-ans = input('Are you an existing user?(Y/n)')
-if ans == 'Y' or ans == 'y':
+def main(suser, db):
+    check = 'y'
+    while check == 'Y' or check == 'y':
+        print('\n')
+        print('1. Add to watched')
+        print('2. Add to watchlist')
+        print('3. Show my watched list')
+        print('4. Show my watchlist')
+        print('5. Show my stats')
+
+        ans = int(input('\nChoose your option : '))
+
+        if ans == 1:
+            ka = 'y'
+            while ka == 'Y' or ka == 'y':
+                mvname = input('Movie name: ')
+                if suser.append_watched(mvname) != None:
+                    mvid, mv = suser.append_watched(mvname)
+                    if mvid not in  db['mvdb']:
+                        db['mvdb'][mvid] = mv
+                    print('Added to watched.')
+                    ka = input('Keep adding?(Y/n)')
+
+        elif ans == 2:
+            ka = 'y'
+            while ka == 'y' or ka == 'Y':
+                mvname = input('Movie name: ')
+                suser.append_watchlist(mvname)
+                print('Added to your watchlist')
+                ka = input('Keep adding?(y/n)')
+
+        elif ans == 3:
+            for i in suser.get_watched():
+                print(i[1])
+        
+        elif ans == 5:
+            suser.show_stats()
+        
+        check = input("\nDo you need to do something else?(Y/n)")
+    
+    pickling_on = open("db.pickle","wb")
+    pickle.dump(db, pickling_on)
+    pickling_on.close()
+        
+    
+
+
+
+def login(db):
     print('\nLogin.')
-    userid = input('Enter userid: ')
-    pword = input('Enter password: ')
-    main()
-else:
+    while True:
+        userid = input('Enter userid: ')
+        pword = input('Enter password: ')
+        if userid in db['users'] and db['users'][userid].pword == pword:
+            session_user = db['users'][userid]
+            break
+        else:
+            print('Wrong userid or password.\nRe-enter.')
+    main(session_user, db)
+
+def sign_up(db):
     print('\nSign Up.')
     name = input('Enter your name: ')
     userid = input('Enter userid: ')
     pword = input('Enter password: ')
     nuser = person(name, userid, pword)
+    db['users'][userid] = nuser
+    main(nuser, db)
 
-def main():
+def developer(db):
+    print(db)
+
+print('\n\t\tWelcome to Movie Tracker\n')
+print('1. Login\n2. Signup\n3. Developer\n')
+while True:
+    logorsign = input('Choose : ')
+    if logorsign == '1':
+        login(db)
+        break
+    elif logorsign == '2':
+        sign_up(db)
+        break
+    elif logorsign == '3':
+        developer(db)
+        break
+    else:
+        print('Enter valid option.')
 
 
--- objects.py
+++ objects.py
@@ -1,21 +1,56 @@
+import imp
 from tmdb import tmdb
+from genre_ids import genres
 
 
-class user:
+class person:
     def __init__(self, name, userid, pword):
         self.name = name
         self.id = userid
         self.pword = pword
         self.watched = []
         self.watchlist = []
+        self.genre_count =  {
+            'Action' : 0,
+            'Adventure' : 0,
+            'Animation' : 0,
+            'Comedy' : 0,
+            'Crime' : 0,
+            'Documentary' : 0,
+            'Drama' : 0,
+            'Family' : 0,
+            'Fantasy' : 0,
+            'History' : 0,
+            'Horror' : 0,
+            'Music' : 0,
+            'Mystery' : 0,
+            'Romance' : 0,
+            'Science Fiction' : 0,
+            'TV Movie' : 0,
+            'Thriller' : 0,
+            'War' : 0,
+            'Western' : 0}
+
     
     def append_watched(self, movie):
         result = tmdb.search(movie).json()['results']
+        if len(result) == 0:
+            '\nPlease check the spelling of the movie.'
+            return None
         j = 1
         for i in result:
-            print(j + '. ' + i['title'])
+            if j > 5:
+                break
+            print(str(j) + '. ' + i['title'] + '  - ' + i['release_date'])
+            j += 1
+        
         idx = int(input('\nEnter index number : '))
-        self.watched.append(result[idx-1]['id']) 
+        list1 = [result[idx-1]['id'], result[idx-1]['title']]
+        self.watched.append(list1)
+        for i in result[idx-1]['genre_ids']:
+            self.genre_count[genres[i]] += 1
+        
+        return result[idx-1]['id'], result[idx-1]
     
     def get_watched(self):
         return self.watched
@@ -40,4 +75,9 @@ class user:
         return self.watchlist
     
     def remove_watchlist(self, movie):
-        self.watchlist.remove(movie) 
+        self.watchlist.remove(movie)
+    
+    def show_stats(self):
+        print('Genre Count: \n')
+        print(self.genre_count)
+    
